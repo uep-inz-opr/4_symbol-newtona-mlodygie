aff832acfc775b539bba22f5978ba7063beb72b5
Kuba <kubaglowacki2@gmail.com>
2022-05-16 23:25:33 +0200

    Zadanie Symbol Newtona

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
 
 n, k = [int(n) for n in input().split()]
-    
-print(math.factorial(n)/(math.factorial(k)*math.factorial(n-k)))
+
+def newton(n,k):
+    return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
+
+print(newton(n, k))
