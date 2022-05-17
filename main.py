56bc106b31fb46b9e1907d72d833ff8d142e911c
Kuba <kubaglowacki2@gmail.com>
2022-05-17 17:05:34 +0200

    Zadanie Symbol Newtona

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import math
 
-n, k = [int(n) for n in input().split()]
+zmienne = map(int, input().split())
+x = list(zmienne)
 
-def newton(n,k):
-    return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
+n = x[0]
+k = x[1]
+nk = n-k
 
-print(newton(n, k))
+wynik_n = []
+wynik_k = []
+wynik_nk = []
+
+while n >= 1:
+    wynik_n.append(n)
+    n = n-1
+
+while k >= 1:
+    wynik_k.append(k)
+    k = k-1
+
+while nk >= 1:
+    wynik_nk.append(nk)
+    nk = nk-1
+
+silnia_n = math.prod(wynik_n)
+silnia_k = math.prod(wynik_k)
+silnia_nk = math.prod(wynik_nk)
+
+newton = (silnia_n/(silnia_k*silnia_nk))
+
+print(int(newton))
